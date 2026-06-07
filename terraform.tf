edc1b9e969915dce8138fda3e8dd57e53ce269f5
rioto.nagano <kingdom950927@gmail.com>
2026-06-07 18:10:24 +0900

    add: 変数の定義

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-/* Uncomment this block to use Terraform Cloud for this tutorial
+/* Uncomment this block to use Terraform Cloud for this tutorial */
 cloud {
-    organization = "organization-name"
+    organization = "terraform_riorio_organization"
     workspaces {
       name = "learn-terraform-sensitive-variables"
     }
 }
-*/
+
 required_providers {
     aws = {
       source  = "hashicorp/aws"
